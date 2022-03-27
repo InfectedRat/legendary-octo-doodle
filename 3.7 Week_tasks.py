8c70b83e7311b6cb0e7eb7ed4c4475b08be50230
Maxim Babichev <birdclock@gmail.com>
2022-03-28 00:08:49 +0300

    команды счет

--- 3.7 Week_tasks.py
+++ 3.7 Week_tasks.py
@@ -6,6 +6,13 @@
 # print(x_list)
 # print(vs)
 
-lst = input().split()
 
-print(lst)
+# n = int(input())
+# team_lst = [input().split(';') for el in range(n)]
+# print(team_lst)
+
+
+n = int(input('Количесвтво игр: '))
+x_list = [input('Ком1;Гол1;Ком2;Гол2: ').split(';') for x in range(n)]
+
+print(x_list)
