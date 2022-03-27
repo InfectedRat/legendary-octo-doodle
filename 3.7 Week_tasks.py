8f60d222f4d1acaf2753156d14c5785305453533
Maxim Babichev <birdclock@gmail.com>
2022-03-27 19:12:16 +0300

    позабыли мы списки... эх....

---
+++ 3.7 Week_tasks.py
@@ -0,0 +1,11 @@
+# n = int(input('kolichestvo igr: '))
+# x_list = [input('kom1;gol1;kom2;gol2: ').split(';') for x in range(n)]
+#
+# vs = [(x[0], x[2]) for x in x_list]
+#
+# print(x_list)
+# print(vs)
+
+lst = input().split()
+
+print(lst)
