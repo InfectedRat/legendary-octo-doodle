d81edeb0d27c6fd898f7a431530b965b284dea0d
Maxim Babichev <birdclock@gmail.com>
2022-03-28 11:20:39 +0300

    i love lists

--- 3.7.2 Week tasks.py
+++ 3.7.2 Week tasks.py
@@ -1,5 +1,26 @@
-need_cipher = str(input()).split()
+buklet = {}
+origin_alph = input()
+origin_list = [x for x in origin_alph]
 
+cipher = input()
+cipher_list = [x for x in cipher]
 
-print(need_cipher)
+for_cipher = input()
+for_cipher_list = [x for x in for_cipher]
 
+for i in range(0, len(origin_list)):
+    buklet[origin_list[i]] = cipher_list[i]  # словарь шифровки
+
+for i, j in enumerate(for_cipher_list):
+    for_cipher_list[i] = buklet[j]
+
+# un_cipher = input()
+
+print(origin_alph)
+print(origin_list)
+
+print(cipher)
+print(cipher_list)
+
+print(for_cipher_list)
+print(buklet)
