7b4da4fa1f4c2563eeda286ce9ed9c4fef4cd255
Maxim Babichev <birdclock@gmail.com>
2022-03-29 00:17:12 +0300

    что-то через списки не прокатило, иду к кортежам

---
+++ 3.7.2 Spell check.py
@@ -0,0 +1,8 @@
+# пробуем через сравнение списков, что видимо не ожидается
+
+cnt_rll_w = int(input())
+really_words = []
+for i in range(cnt_rll_w):
+    really_words.append(input())
+
+print(really_words)
