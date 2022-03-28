0d023d768051b7067b33de38927e5d0747bc5b36
Maxim Babichev <birdclock@gmail.com>
2022-03-28 11:37:45 +0300

    i love lists

--- 3.7.2 Week tasks.py
+++ 3.7.2 Week tasks.py
@@ -1,26 +1,35 @@
-buklet = {}
+# buklet = {}
+# origin_alph = input()
+# origin_list = [x for x in origin_alph]
+#
+# cipher = input()
+# cipher_list = [x for x in cipher]
+#
+# for_cipher = input()
+# for_cipher_list = [x for x in for_cipher]
+#
+# for i in range(0, len(origin_list)):
+#     buklet[origin_list[i]] = cipher_list[i]  # словарь шифровки
+#
+# for i, j in enumerate(for_cipher_list):
+#     for_cipher_list[i] = buklet[j]
+#
+# # un_cipher = input()
+#
+# print(origin_alph)
+# print(origin_list)
+#
+# print(cipher)
+# print(cipher_list)
+#
+# print(for_cipher_list)
+# print(buklet)
+# print(cipher_list.join())
+
+buklet_code = {}
+buklet_encode = {}
+
 origin_alph = input()
-origin_list = [x for x in origin_alph]
-
 cipher = input()
-cipher_list = [x for x in cipher]
-
 for_cipher = input()
-for_cipher_list = [x for x in for_cipher]
-
-for i in range(0, len(origin_list)):
-    buklet[origin_list[i]] = cipher_list[i]  # словарь шифровки
-
-for i, j in enumerate(for_cipher_list):
-    for_cipher_list[i] = buklet[j]
-
-# un_cipher = input()
-
-print(origin_alph)
-print(origin_list)
-
-print(cipher)
-print(cipher_list)
-
-print(for_cipher_list)
-print(buklet)
+un_cipher = input()
