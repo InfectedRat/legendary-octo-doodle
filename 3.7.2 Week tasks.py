3e0cdf9298efa0072b688ed9243ddbd9aa886c86
Maxim Babichev <birdclock@gmail.com>
2022-03-28 17:21:27 +0300

    пиздец , но готово, следующим коммитом будет простой и быстрый вариант)

--- 3.7.2 Week tasks.py
+++ 3.7.2 Week tasks.py
@@ -10,27 +10,37 @@ cipher_list = [x for x in cipher]
 for_cipher = input()
 for_cipher_list = [x for x in for_cipher]
 
+un_cipher = input()
+un_cipher_list = [x for x in un_cipher]
+
 for i in range(0, len(origin_list)):
     buklet[origin_list[i]] = cipher_list[i]  # словарь шифровки
 
 for i in (range(0, len(cipher_list))):
-    un_buklet[cipher_list[i]] = origin_list[i] # словарь расшифровки
+    un_buklet[cipher_list[i]] = origin_list[i]  # словарь расшифровки
 
 for i, j in enumerate(for_cipher_list):
-    for_cipher_list[i] = buklet.get(j)
+    for_cipher_list[i] = buklet.get(j)  # шифруем
+
+for i, j in enumerate(un_cipher_list):
+    un_cipher_list[i] = un_buklet.get(j)  # расшифруем
 
 # un_cipher = input()
 
-print(origin_alph)
-print(origin_list)
-
-print(cipher)
-print(cipher_list)
-
-print(for_cipher_list)
-print(f"Это словарь шифровки {buklet}")
-print(f"Это словарь расшифровки {un_buklet}")
+# print(origin_alph)
+# print(origin_list)
+#
+# print(cipher)
+# print(cipher_list)
+#
+# print(for_cipher_list)
+# print(for_cipher_list)
+# print(f"Это словарь шифровки {buklet}")
+# print(f"Это словарь расшифровки {un_buklet}")
 print("".join(filter(None, for_cipher_list)))
+print("".join(filter(None, un_cipher_list)))
+
+
 
 # buklet_code = {}
 # buklet_encode = {}
