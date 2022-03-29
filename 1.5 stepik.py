d554e31e14aa0be684d144632f4d40987fc14024
Maxim Babichev <birdclock@gmail.com>
2022-03-29 23:43:11 +0300

    новый курс с нуля, тестируем

---
+++ 1.5 stepik.py
@@ -0,0 +1,19 @@
+# age = int(input())
+# print(age+1)
+
+# a, b = map(int, input().split())
+# c = a + b
+# print(c)
+
+# s = int(input())
+# s1 = s / 6
+# s2 = s1 * 4
+# print(int(s1), int(s2), int(s1))
+
+# x, y, z = map(int, input().split())
+# xp = 3
+# yp = xp + 2
+# zp = yp + 7
+# print(x*xp + y*yp + z*zp)
+
+print(*[3 * x + 5 * y + 12 * z for x, y, z in [map(int, input().split())]])
