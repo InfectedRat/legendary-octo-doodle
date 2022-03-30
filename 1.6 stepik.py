3e1571d5575a68876f391a527d0872d32c558d77
Maxim Babichev <birdclock@gmail.com>
2022-03-30 22:00:40 +0300

    end 1.6

--- 1.6 stepik.py
+++ 1.6 stepik.py
@@ -1,9 +1,21 @@
-# print(*[a, b, c for a, b, c in [map(int, input().split()]])
-# print(*[x, y, z for x, y, z in [map(int, input().split())]])
+print(*[a, b, c for a, b, c in [map(int, input().split()]])
+print(*[x, y, z for x, y, z in [map(int, input().split())]])
 
-# print(4249+4247+4247+4247)
+print(4249+4247+4247+4247)
 
-# a, b, c, = map(int, input().split())
-# print(a, b, c, sep=',')
+a, b, c, = map(int, input().split())
+print(a, b, c, sep=',')
 
 print(*input().split(), sep=',')
+
+a = int(input())
+print("%s<%s<%s" % (a - 1, a, a + 1))
+
+a, b, c = [input() for _ in range(0,3)]
+print(a, b, c, sep='---')
+
+separator = input()
+print(1, 2, 3, 4, 5, sep=separator)
+
+print('Гвидо', 'Ван', 'Россум', sep='*', end='-')
+print('Основатель', 'Питона', sep='_', end='!')
