d5a702310f64508bc9de261827c4dd09a7223a6e
Maxim Babichev <birdclock@gmail.com>
2022-03-30 18:26:50 +0300

    продолжаем 1.6

---
+++ 1.6 stepik.py
@@ -0,0 +1,9 @@
+# print(*[a, b, c for a, b, c in [map(int, input().split()]])
+# print(*[x, y, z for x, y, z in [map(int, input().split())]])
+
+# print(4249+4247+4247+4247)
+
+# a, b, c, = map(int, input().split())
+# print(a, b, c, sep=',')
+
+print(*input().split(), sep=',')
