16670a428e4ca4263f53288b299e017c52f9c1e7
Maxim Babichev <birdclock@gmail.com>
2022-04-01 00:20:09 +0300

    1.8 stepik

--- 1.8 stepik.py
+++ 1.8 stepik.py
@@ -1,2 +1,14 @@
-a = int(input())
-print(a>0)
+# a = int(input())
+# print(a>0)
+
+# a = int(input())
+# print(a%2 == 0)
+
+# a = int(input())
+# print(a%10 == 2)
+
+# a, b = map(int, input().split())
+# print(a%7 ==0 and b%7 == 0)
+
+a,b,c = map(int,input().split())
+print(a==b==c)
