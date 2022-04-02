44fd91a2b291fe13576eaafb3a1c3811ec662f32
Maxim Babichev <birdclock@gmail.com>
2022-04-02 23:05:07 +0300

    2.3 end

--- 2.3 start.py
+++ 2.3 start.py
@@ -1,22 +1,22 @@
-# a = input()
-# print(a.upper())
+a = input()
+print(a.upper())
 
-# a = input()
-# print(a.lower())
+a = input()
+print(a.lower())
 
-# s = input()
-# print(s.lower().count('e'))
+s = input()
+print(s.lower().count('e'))
 
-# s = input()
-# print(s.replace('w', "").replace('z', ""))
+s = input()
+print(s.replace('w', "").replace('z', ""))
 
-# s = input()
-# print(s.rfind('a'))
+s = input()
+print(s.rfind('a'))
 
-# s = input()
-# print(int(s.count(' '))+1)
-#
-# print(len(s.split()))
+s = input()
+print(int(s.count(' '))+1)
+
+print(len(s.split()))
 
 s = input()
 print(s.replace(' ', ','))
