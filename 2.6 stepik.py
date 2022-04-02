2042614528b0a965af8da37624ee0b00e87c0b29
Maxim Babichev <birdclock@gmail.com>
2022-04-03 00:22:36 +0300

    2.6

--- 2.6 stepik.py
+++ 2.6 stepik.py
@@ -1,2 +1,5 @@
-instr = input()
-print('Что Вы сказали? {s}? Какое интересное слово'.format(s=instr))
+# instr = input()
+# print('Что Вы сказали? {s}? Какое интересное слово'.format(s=instr))
+
+name, surname = [input() for _ in range(2)]
+print('Здравствуйте, {s} {n}!'.format(n=name, s=surname))
