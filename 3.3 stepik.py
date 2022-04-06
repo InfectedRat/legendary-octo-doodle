cadd2fa0fda4d3265f64cc9e30610781dbe24ffb
Maxim Babichev <birdclock@gmail.com>
2022-04-07 01:14:50 +0300

    3.3 end

--- 3.3 stepik.py
+++ 3.3 stepik.py
@@ -1,67 +1,67 @@
-# n = int(input())
-#
-# if n%3==0 and n%5==0:
-#     print('FizzBuzz')
-# elif n%3==0:
-#     print('Fizz')
-# elif n%5==0:
-#     print('Buzz')
-# else:
-#     print(n)
+n = int(input())
+
+if n%3==0 and n%5==0:
+    print('FizzBuzz')
+elif n%3==0:
+    print('Fizz')
+elif n%5==0:
+    print('Buzz')
+else:
+    print(n)
 
 
-# a, b, c = [int(input()) for _ in range(3)]
-#
-# if a == b == c:
-#     print(3)
-# elif a == b or a == c or b == c:
-#     print(2)
-# else:
-#     print(0)
+a, b, c = [int(input()) for _ in range(3)]
 
-# n = int(input())
-#
-# if n == 1:
-#     print('Январь')
-# elif n == 2:
-#     print('Февраль')
-# elif n == 3:
-#     print('Март')
-# elif n == 4:
-#     print('Апрель')
-# elif n == 5:
-#     print('Май')
-# elif n == 6:
-#     print('Июнь')
-# elif n == 7:
-#     print('Июль')
-# elif n == 8:
-#     print('Август')
-# elif n == 9:
-#     print('Сентябрь')
-# elif n == 10:
-#     print('Октябрь')
-# elif n == 11:
-#     print('Ноябрь')
-# else:
-#     print('Декабрь')
+if a == b == c:
+    print(3)
+elif a == b or a == c or b == c:
+    print(2)
+else:
+    print(0)
 
-# dic = dict(enumerate('Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь'.split(), 1))
-# print(dic[int(input())])
+n = int(input())
 
-# n = int(input())
-# if n<2:
-#     print('Младенец')
-# elif 2<=n<4:
-#     print('Малыш')
-# elif 4<=n<12:
-#     print('Ребенок')
-# elif 12<=n<19:
-#     print('Подросток')
-# elif 19<=n<65:
-#     print('Взрослый человек')
-# elif n>=65:
-#     print('Пожилой человек')
+if n == 1:
+    print('Январь')
+elif n == 2:
+    print('Февраль')
+elif n == 3:
+    print('Март')
+elif n == 4:
+    print('Апрель')
+elif n == 5:
+    print('Май')
+elif n == 6:
+    print('Июнь')
+elif n == 7:
+    print('Июль')
+elif n == 8:
+    print('Август')
+elif n == 9:
+    print('Сентябрь')
+elif n == 10:
+    print('Октябрь')
+elif n == 11:
+    print('Ноябрь')
+else:
+    print('Декабрь')
+
+dic = dict(enumerate('Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь'.split(), 1))
+print(dic[int(input())])
+
+n = int(input())
+if n<2:
+    print('Младенец')
+elif 2<=n<4:
+    print('Малыш')
+elif 4<=n<12:
+    print('Ребенок')
+elif 12<=n<19:
+    print('Подросток')
+elif 19<=n<65:
+    print('Взрослый человек')
+elif n>=65:
+    print('Пожилой человек')
 
 a, b = [float(input()) for _ in range(2)]
 op = str(input())
@@ -78,3 +78,13 @@ elif op == '/' and b != 0:
     print(a/b)
 else:
     print('Неизвестно')
+
+pasw = str(input())
+rpasw = str(input())
+
+if len(pasw)<7:
+    print('Short')
+elif pasw != rpasw:
+    print('Difference')
+else:
+    print('OK')
