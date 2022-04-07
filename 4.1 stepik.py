528852a9de2c2fe956254d304a8105bdf28237f3
Maxim Babichev <birdclock@gmail.com>
2022-04-07 20:44:46 +0300

    4.1 start

---
+++ 4.1 stepik.py
@@ -0,0 +1,16 @@
+# k = 10
+# z = 0
+# while k <= 14:
+#     z = z + k
+#     k += 1
+# print(z)
+
+# k = 1000
+# while k<=2000:
+#     print(k)
+#     k=k+1
+
+k = 6785
+while k>=195:
+    print(k)
+    k=k-5
