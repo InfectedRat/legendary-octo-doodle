49c049326a22e0ebcafbdc3e4727e9103e895f61
Maxim Babichev <birdclock@gmail.com>
2022-04-08 11:41:15 +0300

    4.1 still

--- 4.1 stepik.py
+++ 4.1 stepik.py
@@ -10,7 +10,15 @@
 #     print(k)
 #     k=k+1
 
-k = 6785
-while k>=195:
-    print(k)
-    k=k-5
+# k = 6785
+# while k>=195:
+#     print(k)
+#     k=k-5
+
+a, b = map(int, input().split())
+k=0
+while a<=b:
+    a=a*3
+    b=b*2
+    k=k+1
+print(k)
