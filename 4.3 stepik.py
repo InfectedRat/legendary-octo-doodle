b8c750b92152124a83f4e2bb045e4b2301adabb1
Maxim Babichev <birdclock@gmail.com>
2022-04-11 14:34:43 +0300

    4.3 still

--- 4.3 stepik.py
+++ 4.3 stepik.py
@@ -1,56 +1,56 @@
-# number = 73408
-# m = 0
-# s = 0
-# while number > 0:
-#     last_digit = number % 10
-#     s = s + last_digit
-#     if last_digit > m:
-#         m = last_digit
-#     number = number // 10
-# print(s + m)
+number = 73408
+m = 0
+s = 0
+while number > 0:
+    last_digit = number % 10
+    s = s + last_digit
+    if last_digit > m:
+        m = last_digit
+    number = number // 10
+print(s + m)
 
-# num = int(input())
-# while num > 0:
-#     last_num = num%10
-#     print(last_num)
-#     num = num//10
+num = int(input())
+while num > 0:
+    last_num = num%10
+    print(last_num)
+    num = num//10
 
-# def summ(num):
-#     summ = 1
-#     while num > 0:
-#         last_num = num % 10
-#         summ = summ * last_num
-#         num = num // 10
-#     return summ
-#
-#
-# num = int(input())
-# print(summ(num))
+def summ(num):
+    summ = 1
+    while num > 0:
+        last_num = num % 10
+        summ = summ * last_num
+        num = num // 10
+    return summ
 
-# dl = []
-# while True:
-#     dl.append(int(input()))
-#     if 0 in dl:
-#         break
-# print(sum(dl))
 
-# def minax(num):
-#     minn = 9
-#     maxx = 0
-#     while num > 0:
-#         last_num = num % 10
-#         if last_num > maxx:
-#             maxx = last_num
-#         if last_num < minn:
-#             minn = last_num
-#         num = num // 10
-#     return minn, maxx
-#
-#
-# num = int(input())
-# print(*minax(num), sep='\n')
+num = int(input())
+print(summ(num))
 
-# passw = input()
+dl = []
+while True:
+    dl.append(int(input()))
+    if 0 in dl:
+        break
+print(sum(dl))
+
+def minax(num):
+    minn = 9
+    maxx = 0
+    while num > 0:
+        last_num = num % 10
+        if last_num > maxx:
+            maxx = last_num
+        if last_num < minn:
+            minn = last_num
+        num = num // 10
+    return minn, maxx
+
+
+num = int(input())
+print(*minax(num), sep='\n')
+
+passw = input()
 
 passw='qwerty'
 while 5<=len(passw)<=9:
@@ -67,3 +67,23 @@ while 5 <= len(n) <= 9:
     n = input()
 
 print(l[-1])
+
+count_num=0
+num = int(input())
+while num>0:
+    last_num = num%10
+    if last_num == 7:
+        count_num+=1
+    num=num//10
+
+print(count_num)
+
+print(input().count('7'))
+
+num = int(input())
+while num>0:
+    last_num=num%2
+    print(last_num)
+    num=num//2
+
+
