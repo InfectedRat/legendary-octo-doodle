2bb68f6e2aaf82356022dab9c32679fd3f966e57
Maxim Babichev <birdclock@gmail.com>
2022-04-13 16:03:44 +0300

    5.1 end

---
+++ 5.1 stepik.py
@@ -0,0 +1,15 @@
+print(list(range(0,10)))
+
+print(list(range(12,35)))
+
+print(list(range(25, 170, 8)))
+
+a = list(range(-35, -10, 1))
+b = sorted(a, reverse=True)
+print(b)
+
+print(list(range(-11, -36, -1)))
+
+print(list(range(10, -1, -1)))
+
+print(list(range(1000, 450, -50)))
