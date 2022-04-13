63885b513ee28ab693790e1703a3658cc85ce5d7
Maxim Babichev <birdclock@gmail.com>
2022-04-13 11:33:57 +0300

    4.6 end

--- 4.6 stepik.py
+++ 4.6 stepik.py
@@ -1,36 +1,36 @@
-# n = int(input())
-# i = 2
-# natlst = []
-# while i <= n:
-#     if n % i == 0:
-#         natlst.append(i)
-#     i += 1
-# print(min(natlst))
+n = int(input())
+i = 2
+natlst = []
+while i <= n:
+    if n % i == 0:
+        natlst.append(i)
+    i += 1
+print(min(natlst))
 
-# n = int(input())
-# i = 2
-# while True:
-#     if n%i == 0:
-#         print(i)
-#         break
-#     i+=1
+n = int(input())
+i = 2
+while True:
+    if n%i == 0:
+        print(i)
+        break
+    i+=1
 
-# a, b = [int(input()) for _ in range(2)]
-# while a<=b:
-#     if a%777==0:
-#         break
-#     if a%2!=0 and a%3!=0:
-#         print(a)
-#     a+=1
+a, b = [int(input()) for _ in range(2)]
+while a<=b:
+    if a%777==0:
+        break
+    if a%2!=0 and a%3!=0:
+        print(a)
+    a+=1
 
-# a = int(input())
-# b = int(input())
-# while a<=b:
-#     if a%777==0:
-#         break
-#     if a%2!=0 and a%3!=0:
-#         print(a)
-#     a+=1
+a = int(input())
+b = int(input())
+while a<=b:
+    if a%777==0:
+        break
+    if a%2!=0 and a%3!=0:
+        print(a)
+    a+=1
 
 def nhalf(n):
     n=n/2
@@ -59,3 +59,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+slovo = input()
+i = 0
+
+while len(slovo)-1>=i:
+    if slovo[i] == 'e' or slovo[i] == 'a':
+        print('Ага! Нашлась')
+        break
+    print(f'Текущая буква: {slovo[i]}')
+    i+=1
+else:
+    print('Распечатали все буквы')
+
+
+
+
