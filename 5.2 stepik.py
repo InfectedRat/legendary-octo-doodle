7be1c8f5650c1c52b50bf456d4f9705aa10afb52
Maxim Babichev <birdclock@gmail.com>
2022-04-14 20:11:13 +0300

    5.2 still

--- 5.2 stepik.py
+++ 5.2 stepik.py
@@ -17,11 +17,23 @@
 
 # print([i if not i % 2 else '*' for a in range(51) for i in range(a) if a > 22])
 
-def bartphrase(phrase, n):
-    for i in range(n):
-        print(phrase)
+# def bartphrase(phrase, n):
+#     for i in range(n):
+#         print(phrase)
+#
+# phrase = input()
+# n = int(input())
+#
+# bartphrase(phrase, n)
 
-phrase = input()
-n = int(input())
+a, b = [int(input()) for _ in range(2)]
 
-bartphrase(phrase, n)
+for i in range(a, b+1):
+    if (i%3==0 and i%5==0):
+        print('FizzBuzz')
+    elif i%5==0:
+        print('Buzz')
+    elif i%3==0:
+        print('Fizz')
+    else:
+        print(i)
