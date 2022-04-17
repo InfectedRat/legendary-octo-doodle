1df3e3cd85195c8c9b8abc53da0be0141294dcc2
Maxim Babichev <birdclock@gmail.com>
2022-04-17 21:53:44 +0300

    5.4 end

--- 5.4 stepik.py
+++ 5.4 stepik.py
@@ -1,29 +1,28 @@
-# n = input()
-# a = [0]*10
-#
-# for i in str(n):
-#     digit = int(i)
-#     a[digit]+=1
-#
-# for i in range(10):
-#     if a[i]>0:
-#         print(i, a[i])
+n = input()
+a = [0]*10
 
-# n = input()
-# # ls = [i for i in input()]
-# ls = list(map(int, input().split()))
-# # print(ls)
-# minnls = []
-#
-# for i in range(len(ls)):
-#     minn = min(ls)
-#     minnls.append(minn)
-#     ls.remove(minn)
-#
-# print(*minnls)
+for i in str(n):
+    digit = int(i)
+    a[digit]+=1
 
+for i in range(10):
+    if a[i]>0:
+        print(i, a[i])
 
-# sadasda = int(input())
+n = input()
+# ls = [i for i in input()]
+ls = list(map(int, input().split()))
+# print(ls)
+minnls = []
+
+for i in range(len(ls)):
+    minn = min(ls)
+    minnls.append(minn)
+    ls.remove(minn)
+
+print(*minnls)
+
+sadasda = int(input())
 n = list(map(int,input().split()))
 count = [0]*201
 for i in n:
