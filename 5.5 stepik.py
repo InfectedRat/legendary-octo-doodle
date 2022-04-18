bc9172f74cbefede1d7b6f9517fb5a4d03cf4b89
Maxim Babichev <birdclock@gmail.com>
2022-04-18 12:15:04 +0300

    5.5 still

--- 5.5 stepik.py
+++ 5.5 stepik.py
@@ -1,16 +1,26 @@
-summ=0
-cn=0
-alls=0
+# summ=0
+# cn=0
+# alls=0
+#
+# for i in range(1000, 10000):
+#     cn = i
+#     summ = 0
+#     while cn>0:
+#         summ+=cn%10
+#         cn//=10
+#     if summ == 20:
+#         alls+=i
+#
+#     print(i, summ, alls)
+#
 
-for i in range(1000, 10000):
-    cn = i
-    summ = 0
-    while cn>0:
-        summ+=cn%10
-        cn//=10
-    if summ == 20:
-        alls+=i
 
-    print(i, summ, alls)
 
-1
+def loopss(n):
+    for i in range(1, n+1):
+        for j in range(1,i+1):
+            print(j, end=' ')
+        print()
+
+n = int(input())
+loopss(n)
