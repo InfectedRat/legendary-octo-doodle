17a9ce3c8468450b6aba2c0ea31dc989c28c89ac
Maxim Babichev <birdclock@gmail.com>
2022-04-21 00:19:16 +0300

    5.6 still

--- 5.6 stepik.py
+++ 5.6 stepik.py
@@ -1,26 +1,27 @@
-n = int(input())
-a = []
-summ = 0
+# n = int(input())
+# a = []
+# summ = 0
+# # for i in range(n):
+# #     a.append([0]*n)
+#
+# # for i in range(n):
+# #     b = []
+# #     for j in range(n):
+# #         b.append(list(map(int, input().split())))
+# #     a.append(b)
+#
 # for i in range(n):
-#     a.append([0]*n)
-
+#     a.append(list(map(int, input().split())))
+#
 # for i in range(n):
-#     b = []
+#     su=0
 #     for j in range(n):
-#         b.append(list(map(int, input().split())))
-#     a.append(b)
-
-for i in range(n):
-    a.append(list(map(int, input().split())))
-
-for i in range(n):
-    su=0
-    for j in range(n):
-        if i == j:
-            su = a[i][j]
-            summ = su+summ
-
-print(summ)
+#         if i == j:
+#             su = a[i][j]
+#             summ = su+summ
+#
+# print(summ)
 
 # for i in range(n):
 #     print(a[i])
+
