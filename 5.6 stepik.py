c0a156ded06d010d55e9dea90c635b5b8ce368fa
Maxim Babichev <birdclock@gmail.com>
2022-04-23 20:37:34 +0300

    5.6 still

--- 5.6 stepik.py
+++ 5.6 stepik.py
@@ -1,6 +1,6 @@
-n = int(input())
-a = []
-summ = 0
+# n = int(input())
+# a = []
+# summ = 0
 # for i in range(n):
 #     a.append([0]*n)
 
@@ -10,34 +10,48 @@ summ = 0
 #         b.append(list(map(int, input().split())))
 #     a.append(b)
 
-for i in range(n):
-    a.append(list(map(int, input().split())))
-
-for i in range(n):
-    su=0
-    for j in range(n):
-        if i == j:
-            su = a[i][j]
-            summ = su+summ
+# for i in range(n):
+#     a.append(list(map(int, input().split())))
+#
+# for i in range(n):
+#     su=0
+#     for j in range(n):
+#         if i == j:
+#             su = a[i][j]
+#             summ = su+summ
 
 # print(summ)
 
 # for i in range(n):
 #     print(a[i])
 
-n, m = map(int, input().split())
-a=[]
-
+# n, m = map(int, input().split())
+# a=[]
+#
+# # for i in range(n):
+# #    a.append([0]*n)
+#
 # for i in range(n):
-#    a.append([0]*n)
+#     a.append(list(map(int, input().split())))
+#
+# # for i in range(n):
+# #      print(a[i])
+#
+# for i in range(n-1, -1, -1):
+#     for j in range(m):
+#         print(a[i][j], end=' ')
+#     print()
+#
+#
 
-for i in range(n):
-    a.append(list(map(int, input().split())))
+mas = []
 
-# for i in range(n):
-#      print(a[i])
+for i in range(5):
+    mas.append(list(map(int, input().split())))
 
-for i in range(n):
-    for j in range(m-1, -1, -1):
-        print(a[i][j], end=' ')
-    print()
+for i in range(5):
+    for j in range(5):
+        if mas[i][j] == 1:
+            row = i
+            column = j
+print(abs(2 - row) + abs(2 - column))
