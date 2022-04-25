7c5ab54723310c16e02c20d46ca73276ce5097cb
Maxim Babichev <birdclock@gmail.com>
2022-04-25 14:46:13 +0300

    5.6 still

--- 5.6 stepik.py
+++ 5.6 stepik.py
@@ -44,14 +44,69 @@
 #
 #
 
-mas = []
+# mas = []
+#
+# for i in range(5):
+#     mas.append(list(map(int, input().split())))
+#
+# for i in range(5):
+#     for j in range(5):
+#         if mas[i][j] == 1:
+#             row = i
+#             column = j
+# print(abs(2 - row) + abs(2 - column))
 
-for i in range(5):
-    mas.append(list(map(int, input().split())))
+# n, m = map(int, input().split())
+# a=[]
+#
+# for i in range(n):
+#     a.append(list(map(int, input().split())))
+#
+# for i in range(n):
+#     summ=0
+#     for j in range(m):
+#         summ=a[i][j]+summ
+#     print(summ, end=' ')
+# print()
+#
+# for j in range(m):
+#     summ=0
+#     for i in range(n):
+#         summ=a[i][j]+summ
+#     print(summ, end=' ')
+#
 
-for i in range(5):
-    for j in range(5):
-        if mas[i][j] == 1:
-            row = i
-            column = j
-print(abs(2 - row) + abs(2 - column))
+# n = int(input())
+# a=[]
+# summ1=0
+# summ2=0
+#
+# for i in range(n):
+#     a.append(list(map(int, input().split())))
+#
+# for i in range(n):
+#     for j in range(n):
+#         if i<j:
+#             summ1=a[i][j]+summ1
+#
+# for i in range(n):
+#     for j in range(n):
+#         if i>j:
+#             summ2=a[i][j]+summ2
+#
+# print('Yes' if summ1==summ2 else 'No')
+
+
+n, m = map(int, input().split())
+a = []
+c = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+for i in range(n):
+    s = 0
+    for j in range(m):
+        s += a[i][j]
+    c.append(s)
+print(max(c))
+ind = c.index(max(c))
+print(ind)
