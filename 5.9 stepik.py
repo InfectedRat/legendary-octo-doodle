4cd37fe93ba0e5a6acfd89c9798173bdb4a71b23
Maxim Babichev <birdclock@gmail.com>
2022-04-26 15:36:40 +0300

    5.9 still

--- 5.9 stepik.py
+++ 5.9 stepik.py
@@ -5,6 +5,24 @@
 # a = [i for i in range(1, n+1)]
 # print(a)
 
-n = int(input())
-a = [i for i in range(1, n+1) if n%i==0]
-print(a)
+# n = int(input())
+# a = [i for i in range(1, n+1) if n%i==0]
+# print(a)
+
+# n = int(input())
+# a = [i for i in range(n, (n**2)+1) if i%2!=0]
+# print(a)
+
+a, b = map(int, input().split())
+
+if a<=b:
+    c = [i**2 for i in range(a, b+1)]
+elif a>b:
+    c = [i**3 for i in range(b, a+1)]
+
+# print(c[::-1])
+
+if a<=b:
+    print(c)
+else:
+    print(c[::-1])
