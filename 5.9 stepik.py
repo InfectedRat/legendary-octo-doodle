69481f259fb00d2f06906a21f31cb8436625c105
Maxim Babichev <birdclock@gmail.com>
2022-04-26 14:24:52 +0300

    5.9 still

--- 5.9 stepik.py
+++ 5.9 stepik.py
@@ -1,6 +1,10 @@
 # a = [0 for i in range(100)]
 # print(len(a))
 
+# n = int(input())
+# a = [i for i in range(1, n+1)]
+# print(a)
+
 n = int(input())
-a = [i for i in range(1, n+1)]
+a = [i for i in range(1, n+1) if n%i==0]
 print(a)
