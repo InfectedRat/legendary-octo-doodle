54b3777d17ebaf61b11e55256a9ff4bda17df864
Maxim Babichev <birdclock@gmail.com>
2022-04-26 21:22:28 +0300

    5.9 still

--- 5.9 stepik.py
+++ 5.9 stepik.py
@@ -13,16 +13,26 @@
 # a = [i for i in range(n, (n**2)+1) if i%2!=0]
 # print(a)
 
-a, b = map(int, input().split())
+# a, b = map(int, input().split())
+#
+# if a<=b:
+#     c = [i**2 for i in range(a, b+1)]
+# elif a>b:
+#     c = [i**3 for i in range(b, a+1)]
+#
+# # print(c[::-1])
+#
+# if a<=b:
+#     print(c)
+# else:
+#     print(c[::-1])
 
-if a<=b:
-    c = [i**2 for i in range(a, b+1)]
-elif a>b:
-    c = [i**3 for i in range(b, a+1)]
+st = 'Create a list of the first letters of every word in this string'
+lst1 = st.split()
+lst2 = [lst1[i][0] for i in range(len(lst1))]
+print(lst2)
 
-# print(c[::-1])
-
-if a<=b:
-    print(c)
-else:
-    print(c[::-1])
+# st = 'Create a list of the first letters of every word in this string'
+# st = [i[0] for i in st.split()]
+# for i in st:
+#     print(i, end='')
