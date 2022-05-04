84a32e6cb5ebe27033bdf4a05abd7f848a836ac0
Maxim Babichev <birdclock@gmail.com>
2022-05-04 10:54:35 +0300

    5.10 end

---
+++ 5.10 stepik.py
@@ -0,0 +1,18 @@
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+
+ls = [[i for i in j] for j in vector]
+
+print(ls)
+
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+a = [j for i in vector for j in i]
+print(a)
+
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+ls = [j for j in [i for i in vector]]
+print(ls)
+
+vector = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]]
+ls = [vector[i][j] for i in range(0,6)
+      for j in range(0,3)]
+print(ls)
