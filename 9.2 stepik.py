0f859c5fb733a9991fc5e771e6b8c4d55fd6058b
Maxim Babichev <birdclock@gmail.com>
2022-05-30 22:22:24 +0300

    9.2 start

---
+++ 9.2 stepik.py
@@ -0,0 +1,8 @@
+...
+Melissa Crawford, Lebanon, 17
+Paul Herrera, Kiribati, 17
+Justin Galvan, Namibia, 19
+Mary Estes, Montenegro, 19
+Larry Bray, Brunei Darussalam, 21
+...
+<Имя>, <Страна>, <Возраст>
