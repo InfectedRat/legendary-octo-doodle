80a0eb65ef2071ee276e2a4375bd668fe714be73
Maxim Babichev <birdclock@gmail.com>
2022-06-01 23:42:58 +0300

    9.2 start

--- 9.2 stepik.py
+++ 9.2 stepik.py
@@ -14,3 +14,11 @@ Mary Estes, Montenegro, 19
 Larry Bray, Brunei Darussalam, 21
 ...
 <Имя>, <Страна>, <Возраст>
+
+Melissa Crawford, Lebanon, 17
+Paul Herrera, Kiribati, 17
+Justin Galvan, Namibia, 19
+Mary Estes, Montenegro, 19
+Larry Bray, Brunei Darussalam, 21
+...
+<Имя>, <Страна>, <Возраст>
