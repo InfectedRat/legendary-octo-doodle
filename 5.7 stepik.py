dee1ec01d2fb3a3049a2281b7a45a5cdcd1b86c5
Maxim Babichev <birdclock@gmail.com>
2022-06-02 23:16:52 +0300

    9.2 start

--- 5.7 stepik.py
+++ 5.7 stepik.py
@@ -24,7 +24,18 @@ a=[]
 
 for i in range(n):
     a.append(list((map(int, input().split()))))
+n, m = map(int, input().split())
+a=[]
 
+for i in range(n):
+    a.append(list((map(int, input().split()))))
+
+print(a)
+n, m = map(int, input().split())
+a=[]
+
+for i in range(n):
+    a.append(list((map(int, input().split()))))
 print(a)
 
 # put your python code here
